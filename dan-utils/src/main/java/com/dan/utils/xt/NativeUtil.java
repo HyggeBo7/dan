25f4f6c4cccfe4c500033273e8587cdd7fff2dda
wubodanran <wubodanran@163.com>
2019-04-01 16:33:17 +0800

    更新-2019年4月1日16:32:50

--- dan-utils/src/main/java/com/dan/utils/xt/NativeUtil.java
+++ dan-utils/src/main/java/com/dan/utils/xt/NativeUtil.java
@@ -10,40 +10,45 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public final class NativeUtil {
-	private static Logger logger = LoggerFactory.getLogger(NativeUtil.class);
-	private NativeUtil() {
-	}
+    private static Logger logger = LoggerFactory.getLogger(NativeUtil.class);
 
-	/** 获取机器名 */
-	public static final String getHostName() {
-		String hostName = "";
-		try {
-			hostName = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			logger.error(e.getMessage(), e);
-		}
-		return hostName;
-	}
+    private NativeUtil() {
+    }
 
-	/** 获取网卡序列号 */
-	public static final String getDUID() {
-		String address = "";
-		String command = "cmd.exe /c ipconfig /all";
-		try {
-			Process p = Runtime.getRuntime().exec(command);
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-			while ((line = br.readLine()) != null) {
-				if (line.indexOf("DUID") > 0) {
-					int index = line.indexOf(":");
-					index += 2;
-					address = line.substring(index);
-					break;
-				}
-			}
-			br.close();
-		} catch (IOException e) {
-		}
-		return address;
-	}
+    /**
+     * 获取机器名
+     */
+    public static final String getHostName() {
+        String hostName = "";
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return hostName;
+    }
+
+    /**
+     * 获取网卡序列号
+     */
+    public static final String getDUID() {
+        String address = "";
+        String command = "cmd.exe /c ipconfig /all";
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.indexOf("DUID") > 0) {
+                    int index = line.indexOf(":");
+                    index += 2;
+                    address = line.substring(index);
+                    break;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+        }
+        return address;
+    }
 }
