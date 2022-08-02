248f8d23ec74267ce56ef4d133b7eb8ad5ce9423
wb <dearbo7@163.com>
2022-08-02 17:12:02 +0800

    修改标识枚举

--- bo-common/common-base/src/main/java/top/dearbo/base/enums/DeleteTypeEnum.java
+++ bo-common/common-base/src/main/java/top/dearbo/base/enums/DeleteTypeEnum.java
@@ -3,6 +3,8 @@ package top.dearbo.base.enums;
 import top.dearbo.base.bean.BaseKeyValueEnum;
 
 /**
+ * 是否删除：正常：0，已删除：1
+ *
  * @author wb
  * @date 2022/03/21
  */
@@ -11,18 +13,21 @@ public enum DeleteTypeEnum implements BaseKeyValueEnum {
 	/**
 	 * 存在
 	 */
-	NORMAL(1, "正常"),
+	NORMAL(0, "正常", false),
 
-	REMOVE(0, "已删除");
+	REMOVE(1, "已删除", true);
 
-	DeleteTypeEnum(Integer code, String msg) {
+	DeleteTypeEnum(Integer code, String msg, Boolean deleteFlag) {
 		this.code = code;
 		this.msg = msg;
+		this.deleteFlag = deleteFlag;
 	}
 
 	private final Integer code;
 
 	private final String msg;
+
+	private final Boolean deleteFlag;
 
 	public Integer getCode() {
 		return code;
@@ -30,6 +35,10 @@ public enum DeleteTypeEnum implements BaseKeyValueEnum {
 
 	public String getMsg() {
 		return msg;
+	}
+
+	public Boolean getDeleteFlag() {
+		return deleteFlag;
 	}
 
 	@Override
