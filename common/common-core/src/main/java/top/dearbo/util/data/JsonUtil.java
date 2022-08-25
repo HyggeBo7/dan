160a57ff7d7f74935830ae03b95c19dedfee737c
wb <dearbo7@163.com>
2022-08-25 12:07:28 +0800

    修复gson 序列化map object时， long 类型当做double 处理问题

--- common/common-core/src/main/java/top/dearbo/util/data/JsonUtil.java
+++ common/common-core/src/main/java/top/dearbo/util/data/JsonUtil.java
@@ -1,8 +1,9 @@
-package top.dearbo.util.data;
+﻿package top.dearbo.util.data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.TypeAdapter;
+import com.google.gson.internal.bind.ObjectTypeAdapter;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
@@ -28,211 +29,224 @@ import java.util.Map;
  */
 public class JsonUtil {
 
-    private static final Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+	private static final Logger logger = LoggerFactory.getLogger(JsonUtil.class);
 
-    public static final Gson GSON;
+	public static final Gson GSON;
 
-    public static final Gson GSON_SERIALIZE_NULL;
+	public static final Gson GSON_SERIALIZE_NULL;
 
-    public static final Gson GSON_MAP;
+	public static final Gson GSON_MAP;
 
-    static {
-        GSON = new GsonBuilder()
-                //取消自动转义("=" === "\u003d")
-                .disableHtmlEscaping()
-                .setDateFormat("yyyy-MM-dd HH:mm:ss")
-                .create();
-        GSON_SERIALIZE_NULL = new GsonBuilder()
-                .serializeNulls()
-                .disableHtmlEscaping()
-                .setDateFormat("yyyy-MM-dd HH:mm:ss")
-                .create();
-        GSON_MAP = new GsonBuilder()
-                .disableHtmlEscaping()
-                .enableComplexMapKeySerialization()
-                .registerTypeAdapter(new TypeToken<Map<String, Object>>() {
-                }.getType(), new MapTypeAdapter()).create();
-    }
+	static {
+		GSON = new GsonBuilder()
+				//取消自动转义("=" === "\u003d")
+				.disableHtmlEscaping()
+				.setDateFormat("yyyy-MM-dd HH:mm:ss")
+				.create();
+		GSON_SERIALIZE_NULL = new GsonBuilder()
+				.serializeNulls()
+				.disableHtmlEscaping()
+				.setDateFormat("yyyy-MM-dd HH:mm:ss")
+				.create();
+		GSON_MAP = new GsonBuilder()
+				.disableHtmlEscaping()
+				.enableComplexMapKeySerialization()
+				.setDateFormat("yyyy-MM-dd HH:mm:ss")
+				//这里注册的类型需要和序列化类型一致
+				.registerTypeAdapter(new TypeToken<LinkedHashMap<String, Object>>() {
+				}.getType(), new MapTypeAdapter()).create();
+	}
 
-    /**
-     * 获取GsonBuilder实例
-     *
-     * @return GsonBuilder
-     */
-    public static GsonBuilder builder() {
-        return new GsonBuilder();
-    }
+	/**
+	 * 获取GsonBuilder实例
+	 *
+	 * @return GsonBuilder
+	 */
+	public static GsonBuilder builder() {
+		return new GsonBuilder();
+	}
 
-    public static <T> T fromJson(final String json, final Class<T> cls) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        return GSON.fromJson(json, cls);
-    }
+	public static <T> T fromJson(final String json, final Class<T> cls) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		return GSON.fromJson(json, cls);
+	}
 
-    public static <T> T fromTypeJson(final String json, final Type typeOfT) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        return GSON.fromJson(json, typeOfT);
-    }
+	public static <T> T fromTypeJson(final String json, final Type typeOfT) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		return GSON.fromJson(json, typeOfT);
+	}
 
-    /**
-     * 转成list
-     *
-     * @param json  json字符串
-     * @param clazz 指定泛型类
-     * @return 返回泛型
-     */
-    public static <T> List<T> fromListJson(final String json, final Class<T> clazz) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        return GSON.fromJson(json, TypeToken.getParameterized(List.class, clazz).getType());
-    }
+	/**
+	 * 转成list
+	 *
+	 * @param json  json字符串
+	 * @param clazz 指定泛型类
+	 * @return 返回泛型
+	 */
+	public static <T> List<T> fromListJson(final String json, final Class<T> clazz) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		return GSON.fromJson(json, TypeToken.getParameterized(List.class, clazz).getType());
+	}
 
-    /**
-     * 字符串转对象
-     *
-     * @param json      字符串
-     * @param clazzType 转换的对象:Pagination.class <T>
-     * @param clazz     泛型类型: User.cass
-     * @param <T>       Pagination<User>
-     * @return Pagination<User>
-     */
-    public static <T> T fromGenericJson(String json, Class<?> clazzType, Class<?> clazz) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        Type objectType = type(clazzType, clazz);
-        return GSON.fromJson(json, objectType);
-    }
+	/**
+	 * 字符串转对象
+	 *
+	 * @param json      字符串
+	 * @param clazzType 转换的对象:Pagination.class <T>
+	 * @param clazz     泛型类型: User.cass
+	 * @param <T>       Pagination<User>
+	 * @return Pagination<User>
+	 */
+	public static <T> T fromGenericJson(String json, Class<?> clazzType, Class<?> clazz) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		Type objectType = type(clazzType, clazz);
+		return GSON.fromJson(json, objectType);
+	}
 
-    /**
-     * 将json转为Map
-     *
-     * @param json json
-     * @return Map<String, Object>
-     */
-    public static Map<String, Object> toMap(String json) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        return GSON_MAP.fromJson(json, new TypeToken<LinkedHashMap<String, Object>>() {
-        }.getType());
-    }
+	/**
+	 * 将json转为Map
+	 *
+	 * @param json json
+	 * @return Map<String, Object>
+	 */
+	public static Map<String, Object> toMap(String json) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		return GSON_MAP.fromJson(json, new TypeToken<LinkedHashMap<String, Object>>() {
+		}.getType());
+	}
 
-    /**
-     * 将json转为Map List
-     * 解决整数为浮点数
-     *
-     * @param json json
-     * @return List<T>
-     */
-    public static List<Map<String, Object>> toMapList(String json) {
-        if (StringUtils.isBlank(json)) {
-            return null;
-        }
-        return GSON_MAP.fromJson(json, new TypeToken<List<Map<String, Object>>>() {
-        }.getType());
-    }
+	/**
+	 * 将json转为Map List
+	 * 解决整数为浮点数
+	 *
+	 * @param json json
+	 * @return List<T>
+	 */
+	public static List<Map<String, Object>> toMapList(String json) {
+		if (StringUtils.isBlank(json)) {
+			return null;
+		}
+		return GSON_MAP.fromJson(json, new TypeToken<List<LinkedHashMap<String, Object>>>() {
+		}.getType());
+	}
 
-    /**
-     * 将object对象转成json字符串
-     *
-     * @param obj 对象
-     * @return json String
-     */
-    public static String toJson(Object obj) {
-        if (obj == null) {
-            return null;
-        }
-        return GSON.toJson(obj);
-    }
+	/**
+	 * 将object对象转成json字符串
+	 *
+	 * @param obj 对象
+	 * @return json String
+	 */
+	public static String toJson(Object obj) {
+		if (obj == null) {
+			return null;
+		}
+		return GSON.toJson(obj);
+	}
 
-    public static String toJsonSerializeNull(Object obj) {
-        if (obj == null) {
-            return null;
-        }
-        return GSON_SERIALIZE_NULL.toJson(obj);
-    }
+	public static String toJsonSerializeNull(Object obj) {
+		if (obj == null) {
+			return null;
+		}
+		return GSON_SERIALIZE_NULL.toJson(obj);
+	}
 
-    public static ParameterizedType type(final Class<?> raw, final Type... args) {
-        return new ParameterizedType() {
-            @Override
-            public Type getRawType() {
-                return raw;
-            }
+	public static ParameterizedType type(final Class<?> raw, final Type... args) {
+		return new ParameterizedType() {
+			@Override
+			public Type getRawType() {
+				return raw;
+			}
 
-            @Override
-            public Type[] getActualTypeArguments() {
-                return args;
-            }
+			@Override
+			public Type[] getActualTypeArguments() {
+				return args;
+			}
 
-            @Override
-            public Type getOwnerType() {
-                return null;
-            }
-        };
-    }
+			@Override
+			public Type getOwnerType() {
+				return null;
+			}
+		};
+	}
 
-    public static class MapTypeAdapter extends TypeAdapter<Object> {
+	public static class MapTypeAdapter extends TypeAdapter<Object> {
 
-        @Override
-        public Object read(JsonReader in) throws IOException {
-            JsonToken token = in.peek();
-            switch (token) {
-                case BEGIN_ARRAY:
-                    List<Object> list = new ArrayList<Object>();
-                    in.beginArray();
-                    while (in.hasNext()) {
-                        list.add(read(in));
-                    }
-                    in.endArray();
-                    return list;
+		@Override
+		public Object read(JsonReader in) throws IOException {
+			JsonToken token = in.peek();
+			switch (token) {
+				case BEGIN_ARRAY:
+					List<Object> list = new ArrayList<Object>();
+					in.beginArray();
+					while (in.hasNext()) {
+						list.add(read(in));
+					}
+					in.endArray();
+					return list;
 
-                case BEGIN_OBJECT:
-                    Map<String, Object> map = new LinkedHashMap<String, Object>();
-                    in.beginObject();
-                    while (in.hasNext()) {
-                        map.put(in.nextName(), read(in));
-                    }
-                    in.endObject();
-                    return map;
+				case BEGIN_OBJECT:
+					Map<String, Object> map = new LinkedHashMap<String, Object>();
+					in.beginObject();
+					while (in.hasNext()) {
+						map.put(in.nextName(), read(in));
+					}
+					in.endObject();
+					return map;
 
-                case STRING:
-                    return in.nextString();
-                case NUMBER:
-                    //将其作为一个字符串读取出来
-                    String numberStr = in.nextString();
-                    //返回的numberStr不会为null
-                    if (numberStr.contains("e") || numberStr.contains("E")) {
-                        return Double.parseDouble(numberStr);
-                    }
-                    if (numberStr.contains(".")) {
-                        BigDecimal bigDecimal = new BigDecimal(numberStr);
-                        if (numberStr.endsWith(".0")) {
-                            return bigDecimal.longValue();
-                        }
-                        return bigDecimal.doubleValue();
-                    }
-                    return Long.parseLong(numberStr);
-                case BOOLEAN:
-                    return in.nextBoolean();
-                case NULL:
-                    in.nextNull();
-                    return null;
-                default:
-                    throw new IllegalStateException();
-            }
-        }
+				case STRING:
+					return in.nextString();
+				case NUMBER:
+					//将其作为一个字符串读取出来
+					String numberStr = in.nextString();
+					//返回的numberStr不会为null
+					if (numberStr.contains("e") || numberStr.contains("E")) {
+						return Double.parseDouble(numberStr);
+					}
+					if (numberStr.contains(".")) {
+						BigDecimal bigDecimal = new BigDecimal(numberStr);
+						/*if (numberStr.endsWith(".0")) {
+							return bigDecimal.longValue();
+						}*/
+						return bigDecimal.doubleValue();
+					}
+					return Long.parseLong(numberStr);
+				case BOOLEAN:
+					return in.nextBoolean();
+				case NULL:
+					in.nextNull();
+					return null;
+				default:
+					throw new IllegalStateException();
+			}
+		}
 
-        @Override
-        public void write(JsonWriter out, Object value) throws IOException {
-            // 序列化无需实现
-        }
+		@Override
+		public void write(JsonWriter out, Object value) throws IOException {
+			if (value == null) {
+				out.nullValue();
+				return;
+			}
+			//noinspection unchecked
+			TypeAdapter<Object> typeAdapter = (TypeAdapter<Object>) GSON_MAP.getAdapter(value.getClass());
+			if (typeAdapter instanceof ObjectTypeAdapter) {
+				out.beginObject();
+				out.endObject();
+				return;
+			}
+			typeAdapter.write(out, value);
+		}
 
-    }
+	}
 
 }
 
