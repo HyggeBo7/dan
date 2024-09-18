6f7f67c82c381af18f0613dc5c60da5cc7a0210e
wb <dearbo7@163.com>
2024-09-18 10:54:52 +0800

    更新压缩

--- common/common-core/src/main/java/top/dearbo/util/file/FileZipUtil.java
+++ common/common-core/src/main/java/top/dearbo/util/file/FileZipUtil.java
@@ -1,6 +1,7 @@
 package top.dearbo.util.file;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +19,59 @@ import java.util.zip.ZipOutputStream;
  */
 public class FileZipUtil {
 
-	private static final Logger logger = LoggerFactory.getLogger(FileZipUtil.class);
+	private static final Logger log = LoggerFactory.getLogger(FileZipUtil.class);
+
+	/**
+	 * 递归压缩方法
+	 * @param sourceFile 源文件
+	 * @param zos        zip输出流
+	 * @param name       压缩后的名称
+	 * @param KeepDirStructure  是否保留原来的目录结构,true:保留目录结构;
+	 *                          false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
+	 * @throws Exception
+	 */
+	public static void compress(File sourceFile, ZipOutputStream zos, String name, boolean KeepDirStructure) {
+		byte[] buf = new byte[2048];
+		try {
+			if (sourceFile.isFile()) {
+				// 向zip输出流中添加一个zip实体，构造器中name为zip实体的文件的名字
+				zos.putNextEntry(new ZipEntry(name));
+				// copy文件到zip输出流中
+				int len;
+				FileInputStream in = new FileInputStream(sourceFile);
+				while ((len = in.read(buf)) != -1) {
+					zos.write(buf, 0, len);
+				}
+				zos.closeEntry();
+				in.close();
+			} else {
+				File[] listFiles = sourceFile.listFiles();
+				if (listFiles != null && listFiles.length > 0) {
+					for (File file : listFiles) {
+						// 判断是否需要保留原来的文件结构
+						if (KeepDirStructure) {
+							// 注意：file.getName()前面需要带上父文件夹的名字加一斜杠,
+							// 不然最后压缩包中就不能保留原来的文件结构,即：所有文件都跑到压缩包根目录下了
+							String dirName = StringUtils.isBlank(name) ? file.getName() : name + "/" + file.getName();
+							compress(file, zos, dirName, KeepDirStructure);
+						} else {
+							compress(file, zos, file.getName(), KeepDirStructure);
+						}
+					}
+				} else {
+					// 需要保留原来的文件结构时,需要对空文件夹进行处理
+					if (KeepDirStructure && StringUtils.isNotBlank(name)) {
+						// 空文件夹的处理
+						zos.putNextEntry(new ZipEntry(name + "/"));
+						// 没有文件，不需要文件的copy
+						zos.closeEntry();
+					}
+				}
+			}
+		} catch (Exception e) {
+			log.error("zip file failed", e);
+		}
+	}
 
 	/**
 	 * 压缩文件-文件流
@@ -54,7 +107,7 @@ public class FileZipUtil {
 			zipFiles(srcFiles, out, "");
 			out.close();
 		} catch (IOException e) {
-			logger.error("zipFileWithTier zipPath：{} error", zipPath, e);
+			log.error("zipFileWithTier zipPath：{} error", zipPath, e);
 		}
 	}
 
@@ -69,7 +122,7 @@ public class FileZipUtil {
 			zipFiles(srcFiles, zip, "");
 			zip.closeEntry();
 		} catch (IOException e) {
-			logger.error("zipFileWithTier srcFiles：{} error", srcFiles, e);
+			log.error("zipFileWithTier srcFiles：{} error", srcFiles, e);
 		}
 	}
 
@@ -156,7 +209,7 @@ public class FileZipUtil {
 			fos.write(bytes);
 			return true;
 		} catch (IOException e) {
-			logger.error("read file error path：{}", file.getPath(), e);
+			log.error("read file error path：{}", file.getPath(), e);
 		}
 		return false;
 	}
